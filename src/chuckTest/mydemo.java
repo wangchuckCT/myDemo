ffd1aef671d58a82428a48cdb5e70f9aa0624e34
CT-01 <e5w133@126.com>
2018-04-22 16:06:12 +0800

    chuckTest

---
+++ src/chuckTest/mydemo.java
@@ -0,0 +1,54 @@
+package chuckTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import myPeople.People;
+import myPeople.mygun;
+
+public class mydemo {
+
+	private int myName;
+
+	public int getMyName() {
+		return myName;
+	}
+
+	public void setMyName(int myName) {
+		this.myName = myName;
+	}
+
+	public String GetData() {
+
+		return "hello chuck";
+
+	}
+	
+	/**
+ * 
+ * @param args
+ */
+	public static void main(String args[]) {
+		
+		System.out.println("hello chuck ");
+		
+		List<String> names=new ArrayList<>();
+		names.add("chuck");
+		names.add("daci");
+		
+		 int m = names.indexOf("daci");
+		System.out.println(m);
+		
+		 boolean p = names.remove("pp");
+		 System.out.println(p);
+		 
+		// People pp =new People();
+		 
+		 
+	}
+	
+}
+
+
+
+
---
+++ src/myPeople/mygun.java
@@ -0,0 +1,24 @@
+package myPeople;
+
+public class mygun {
+
+	private String gunName;
+	
+	private String gunStyle;
+
+	public String getGunName() {
+		return gunName;
+	}
+
+	public void setGunName(String gunName) {
+		this.gunName = gunName;
+	}
+
+	public String getGunStyle() {
+		return gunStyle;
+	}
+
+	public void setGunStyle(String gunStyle) {
+		this.gunStyle = gunStyle;
+	}
+}
