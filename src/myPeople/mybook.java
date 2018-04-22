7197b59baa496ce3911d3c3bc42791da447d3f79
CT-01 <e5w133@126.com>
2018-04-22 16:20:04 +0800

    新增book文件

---
+++ src/myPeople/mybook.java
@@ -0,0 +1,16 @@
+package myPeople;
+
+public class mybook {
+
+	private String bookName;
+
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	
+	
+}
